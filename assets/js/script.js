2ca3e36930f30754995c4c40e085835faef86195
Viviana Molina <viviana.a.molina@gmail.com>
2024-03-02 04:37:00 -0300

    Formatted documents

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,25 +4,25 @@ import Mascota from './mascota.js';
 
 document.getElementsByTagName("button")[0].addEventListener("click", (e) => {
     e.preventDefault();
-    let propietario1 = new Propietario (document.getElementById("propietario").value,
-    document.getElementById("direccion").value,
-    document.getElementById("telefono").value);
+    let propietario1 = new Propietario(document.getElementById("propietario").value,
+        document.getElementById("direccion").value,
+        document.getElementById("telefono").value);
 
-    let mascota1 = new Mascota (document.getElementById("propietario").value,
+    let mascota1 = new Mascota(document.getElementById("propietario").value,
         document.getElementById("direccion").value,
         document.getElementById("telefono").value,
         document.getElementById("tipo").value,
         document.getElementById("nombreMascota").value,
         document.getElementById("enfermedad").value);
-        console.log(mascota1);
+    console.log(mascota1);
 
     const resultElemUl = document.getElementById('resultado').children[0];
-            
-        const listaDatosPropietario = document.createElement("li");
-        const listaDatosMascota = document.createElement("li");
 
-        listaDatosPropietario.innerHTML = mascota1.datosPropietario();
-        listaDatosMascota.innerHTML = (mascota1.getTipoAnimal() + mascota1.getNombreEnfermedadMascota()) ;
-        resultElemUl.appendChild(listaDatosPropietario);
-        resultElemUl.appendChild(listaDatosMascota);
+    const listaDatosPropietario = document.createElement("li");
+    const listaDatosMascota = document.createElement("li");
+
+    listaDatosPropietario.innerHTML = mascota1.datosPropietario();
+    listaDatosMascota.innerHTML = (mascota1.getTipoAnimal() + mascota1.getNombreEnfermedadMascota());
+    resultElemUl.appendChild(listaDatosPropietario);
+    resultElemUl.appendChild(listaDatosMascota);
 })
